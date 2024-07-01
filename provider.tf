5f1f7ca0168a5cbcb8e21e9c4cc12091898f04a6
Oluwasegun Sorunke <kheysautos@gmail.com>
2024-07-01 12:05:52 -0700

    updated the provider with sudo permission

--- provider.tf
+++ provider.tf
@@ -14,10 +14,10 @@ resource "aws_instance" "ubuntu-apache" {
 
    user_data = <<-EOF
               #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              systemctl start apache2
-              systemctl enable apache2
+              sudo apt-get update
+              sudo apt-get install -y apache2
+              sudo systemctl start apache2
+              sudo systemctl enable apache2
               echo '<html><h1>Hello for test purpose, Terraform!</h1></html>' > /var/www/html/index.html
               EOF
 }
