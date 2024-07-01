752c266916d0b47141f246a0f03cc8f6e72e6cab
Oluwasegun Sorunke <kheysautos@gmail.com>
2024-06-30 21:45:50 -0700

    adding vpc security group parameters to provider.tf

--- provider.tf
+++ provider.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "ubuntu-apache" {
   ami           = var.ami_id
   instance_type = "t2.micro"
   key_name      = "ubuntukey"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
    tags = {
      Name = "ubuntu-apache"
